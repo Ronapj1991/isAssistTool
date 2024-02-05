8dd76c38b774cc29f438d1a85f6e72f93bccf700
Ron Javellana <Ronapj1991@gmail.com>
2024-02-05 16:42:15 +0000

    add filter by analyst or automated incident classification

--- app/models/reported_incident.rb
+++ app/models/reported_incident.rb
@@ -1,23 +1,24 @@
 class ReportedIncident < ApplicationRecord
-    has_many :comments, dependent: :destroy
+  require 'csv'
+  has_many :comments, dependent: :destroy
     
-    CSV_HEADER = %w[Incident-ID Resolution Reporter Sender Subject Resolved_by Themis_Confidence Comments]
-    def self.as_csv
-      CSV.generate do |csv|
-        csv << CSV_HEADER
-        all.each do |incident|
-          csv << [
-            incident.incident_id, 
-            incident.resolution, 
-            incident.reported_by, 
-            incident.sender, 
-            incident.subject, 
-            incident.resolved_by, 
-            incident.themis_confidence,
-            incident.comments.pluck(:content).join(', ')
-          ]
-        end
+  CSV_HEADER = %w[Incident-ID Resolution Reporter Sender Subject Resolved_by Themis_Confidence Comments]
+  def self.as_csv
+    CSV.generate do |csv|
+      csv << CSV_HEADER
+      all.each do |incident|
+        csv << [
+          incident.incident_id, 
+          incident.resolution, 
+          incident.reported_by, 
+          incident.sender, 
+          incident.subject, 
+          incident.resolved_by, 
+          incident.themis_confidence,
+          incident.comments.pluck(:content).join(', ')
+        ]
       end
     end
+  end
     
 end
